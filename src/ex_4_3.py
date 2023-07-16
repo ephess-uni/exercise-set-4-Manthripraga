11268498fad550e181d1958ce67a98f9a5579e6f
Manthripraga <136190794+Manthripraga@users.noreply.github.com>
2023-07-16 14:42:18 -0500

    Update ex_4_3.py
    complete

--- src/ex_4_3.py
+++ src/ex_4_3.py
@@ -1,4 +1,3 @@
- """ ex_4_3.py """
 import os
 
 try:
@@ -10,23 +9,51 @@ except ImportError:
     from ex_4_2 import logstamp_to_datetime
     from util import get_data_file_path
 
- 
+# Use this FILENAME variable to test your function.
 FILENAME = get_data_file_path("messages.log")
- 
+# >>>> DO NOT MODIFY CODE ABOVE <<<<
 
 
 def time_between_shutdowns(logfile):
-   
-    shutdown_events = get_shutdown_events(logfile)
+    
+    shutdowns = get_shutdown_events(logfile)
+    
+    sh1 = shutdowns[0]
+    
+    sh2 = shutdowns[-1]
+       
+    sh1_date = logstamp_to_datetime(sh1.split()[1])
+    
+    sh2_date = logstamp_to_datetime(sh2.split()[1])
+    
+    dfr = sh2_date-sh1_date
+    
+    return dfr
 
- 
-    first_shutdown = logstamp_to_datetime(shutdown_events[0].split()[1])
-    last_shutdown = logstamp_to_datetime(shutdown_events[-1].split()[1])
 
- 
-    return abs(last_shutdown - first_shutdown)
-
- 
+# >>>> The code below will call your function and print the results
 if __name__ == "__main__":
     print(f'{time_between_shutdowns(FILENAME)=}')
+	
 
+
+"""ex_5_0.py"""
+
+
+def line_count(infile):
+    with open(infile, "r") as f:
+        print(len(f.readlines()))
+        
+
+
+if __name__ == "__main__":
+    # get the utility function for path discovery
+    try:
+        from src.util import get_repository_root
+    except ImportError:
+        from util import get_repository_root
+
+    # Test line_count with a file from the data directory
+    data_directory = get_repository_root() / "data"
+    line_count(data_directory / "ex_5_2-data.csv")
+	
